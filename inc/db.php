09ce27f8d78d032d1d1cf5b01ac3bb549b92292e
Dotsquares <30402051+dotsquares-support@users.noreply.github.com>
2022-09-15 11:06:38 +0530

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "shopifyapp";
-$password = "0oMqGdNAkJq3Ec";
-$db = "sociallinksapp";
+$username = "######";
+$password = "#######";
+$db = "#######";
 $conn = mysqli_connect($servername, $username, $password, $db);
 if (!$conn) {
 die("Connection Error: " . mysqli_connect_error());
