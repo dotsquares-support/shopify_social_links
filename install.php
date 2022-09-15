2f9e043d4b12ee09bcfaa3fa7ab0b1dcfc0f36a6
Dotsquares <30402051+dotsquares-support@users.noreply.github.com>
2022-09-15 11:07:33 +0530

    Update install.php

--- install.php
+++ install.php
@@ -1,9 +1,9 @@
 <?php
 $shop = $_GET['shop'];
 
-$api_key = "1e69a73193cec5e4046fb3bc50589954";
+$api_key = "################";
 $scopes = "read_orders, read_content, write_content, write_products, write_script_tags, read_themes, write_themes";
-$redirect_uri = "https://shopifyapp.24livehost.com/sociallinks/token.php";
+$redirect_uri = "https://example.com/sociallinks/token.php";
 
 $install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
 
